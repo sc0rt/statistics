346a53fb02ab47ed67d24f70c0bf8dd017c1548c
Scott Garland <scott.garland@ontariotechu.net>
2020-02-21 15:00:01 -0500

    Update EconMonteCarlo.py

--- EconMonteCarlo.py
+++ EconMonteCarlo.py
@@ -1,8 +1,8 @@
 import datetime as dt
-import numpy as np  
-import pandas as pd  
-import pandas_datareader as pdr  
-import matplotlib.pyplot as plt  
+import numpy as np 
+import pandas as pd
+import pandas_datareader as pdr
+import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 start = dt.datetime(2018,1,1)
