7ea39d8b14cda803240fb0dddb4304bfe277e2cd
Scott Garland <scott.garland@uoit.net>
2020-03-21 03:22:57 -0400

    Update LCBOMonteCarlo.py

--- LCBOMonteCarlo.py
+++ LCBOMonteCarlo.py
@@ -1,6 +1,3 @@
-# Credit to Lester Leong and his "Python Risk Management: Monte Carlo Simulations" 
-# article which helped with starting off and understanding the material.
-
 import datetime as dt
 import numpy as np 
 import pandas as pd
